4fa32f4f70ca3eebd7abef49f82d0000bb06bc48
Ben Christianson <thhuntertgm@gmail.com>
2022-12-01 14:16:53 -0600

    Empty project

--- src/main/java/com/archeryonly/quiver/lanes/LaneRepository.java
+++
@@ -1,6 +0,0 @@
-package com.archeryonly.quiver.lanes;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LaneRepository extends JpaRepository<Lane, UUID> {}
--- src/main/java/com/archeryonly/quiver/reservations/ReservationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.archeryonly.quiver.reservations;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, UUID> {}
