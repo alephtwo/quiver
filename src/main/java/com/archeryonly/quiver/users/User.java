f4bd54f42d4c0d836c2e043789010b12325e0f8b
Ben Christianson <thhuntertgm@gmail.com>
2022-12-01 01:10:29 -0600

    Add stub entities and repositories

---
+++ src/main/java/com/archeryonly/quiver/users/User.java
@@ -0,0 +1,26 @@
+package com.archeryonly.quiver.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private UUID id;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+}
