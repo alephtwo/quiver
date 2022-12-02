ac877670a12cc417bc9428fe150664d40f432d1d
Ben Christianson <thhuntertgm@gmail.com>
2022-12-01 19:55:34 -0600

    Cut back lanes

--- test/quiver/lanes_test.exs
+++ test/quiver/lanes_test.exs
@@ -30,30 +30,5 @@ defmodule Quiver.LanesTest do
     test "create_lane/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Lanes.create_lane(@invalid_attrs)
     end
-
-    test "update_lane/2 with valid data updates the lane" do
-      lane = lane_fixture()
-      update_attrs = %{number: 43}
-
-      assert {:ok, %Lane{} = lane} = Lanes.update_lane(lane, update_attrs)
-      assert lane.number == 43
-    end
-
-    test "update_lane/2 with invalid data returns error changeset" do
-      lane = lane_fixture()
-      assert {:error, %Ecto.Changeset{}} = Lanes.update_lane(lane, @invalid_attrs)
-      assert lane == Lanes.get_lane!(lane.id)
-    end
-
-    test "delete_lane/1 deletes the lane" do
-      lane = lane_fixture()
-      assert {:ok, %Lane{}} = Lanes.delete_lane(lane)
-      assert_raise Ecto.NoResultsError, fn -> Lanes.get_lane!(lane.id) end
-    end
-
-    test "change_lane/1 returns a lane changeset" do
-      lane = lane_fixture()
-      assert %Ecto.Changeset{} = Lanes.change_lane(lane)
-    end
   end
 end
