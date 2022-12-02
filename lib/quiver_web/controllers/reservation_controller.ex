a0f9ecd784480c46d59bee1aa92246911bf1df75
Ben Christianson <thhuntertgm@gmail.com>
2022-12-02 16:50:44 -0600

    Time zones are my nemesis

--- lib/quiver_web/controllers/reservation_controller.ex
+++ lib/quiver_web/controllers/reservation_controller.ex
@@ -15,7 +15,7 @@ defmodule QuiverWeb.ReservationController do
   end
 
   def create(conn, %{"reservation" => reservation_params}) do
-    case Reservations.create_reservation(reservation_params) do
+    case Reservations.create_reservation(adjust_tz(reservation_params)) do
       {:ok, reservation} ->
         conn
         |> put_flash(:info, "Reservation created successfully.")
@@ -27,12 +27,12 @@ defmodule QuiverWeb.ReservationController do
   end
 
   def show(conn, %{"id" => id}) do
-    reservation = Reservations.get_reservation!(id)
+    reservation = Reservations.get_reservation!(id) |> shift_tz_to_la()
     render(conn, "show.html", reservation: reservation)
   end
 
   def edit(conn, %{"id" => id}) do
-    reservation = Reservations.get_reservation!(id)
+    reservation = Reservations.get_reservation!(id) |> shift_tz_to_la()
     changeset = Reservations.change_reservation(reservation)
     render(conn, "edit.html", reservation: reservation, changeset: changeset)
   end
@@ -40,7 +40,7 @@ defmodule QuiverWeb.ReservationController do
   def update(conn, %{"id" => id, "reservation" => reservation_params}) do
     reservation = Reservations.get_reservation!(id)
 
-    case Reservations.update_reservation(reservation, reservation_params) do
+    case Reservations.update_reservation(reservation, adjust_tz(reservation_params)) do
       {:ok, reservation} ->
         conn
         |> put_flash(:info, "Reservation updated successfully.")
@@ -58,5 +58,27 @@ defmodule QuiverWeb.ReservationController do
     conn
     |> put_flash(:info, "Reservation deleted successfully.")
     |> redirect(to: Routes.reservation_path(conn, :index))
+  end
+
+  defp adjust_tz(reservation_params) do
+    reservation_params
+    |> Map.update!("starts_at", &parse_date_as_los_angeles/1)
+    |> Map.update!("ends_at", &parse_date_as_los_angeles/1)
+    |> IO.inspect()
+  end
+
+  defp shift_tz_to_la(reservation) do
+    %Reservation{
+      reservation
+      | starts_at: reservation.starts_at |> DateTime.shift_zone!("America/Los_Angeles"),
+        ends_at: reservation.ends_at |> DateTime.shift_zone!("America/Los_Angeles")
+    }
+  end
+
+  defp parse_date_as_los_angeles(str) do
+    # append the seconds so it's a valid iso 8601 time
+    (str <> ":00")
+    |> NaiveDateTime.from_iso8601!()
+    |> DateTime.from_naive!("America/Los_Angeles")
   end
 end
