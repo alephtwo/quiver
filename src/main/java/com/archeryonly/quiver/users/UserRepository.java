b2356ee4971070dd87619dbd113532636ca9a9b6
Ben Christianson <thhuntertgm@gmail.com>
2022-12-02 20:43:20 -0600

    Remove User for the time being

--- src/main/java/com/archeryonly/quiver/users/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.archeryonly.quiver.users;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, UUID> {}
