029e77fc38bb7b97884fa8afa516dfe102666f16
Ben Christianson <thhuntertgm@gmail.com>
2022-12-21 13:14:31 -0600

    Add header

--- frontend/src/app/Application.tsx
+++ frontend/src/app/Application.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { useState } from 'react';
 import Container from '@mui/material/Container';
 import { Navigation } from './Navigation';
+import { Typography } from '@mui/material';
+import { Header } from './Header';
 
 export function Application(): JSX.Element {
   const [tab, setTab] = useState(0);
 
   return (
     <>
-      <Container sx={styles.container}></Container>
-
+      <Header />
       <Navigation tab={tab} setTab={setTab} />
     </>
   );
---
+++ frontend/src/app/Header.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+import * as React from 'react';
+
+export function Header(): JSX.Element {
+  return (
+    <Typography fontWeight="bold" variant="h2" align="center">
+      Quiver
+    </Typography>
+  );
+}
